3a5f847194d5985fab7109db26958559198da2a9
dam <dam@203P17>
2023-02-07 09:40:47 +0100

    EjercicioGit1 07/02/2023
    2 Souts, 1 Tag

--- src/ejerciciogit1/Principal1.java
+++ src/ejerciciogit1/Principal1.java
@@ -14,7 +14,7 @@ public class Principal1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola");
+        System.out.println("Hola2");
         System.out.println("Hola1");
     }
     
